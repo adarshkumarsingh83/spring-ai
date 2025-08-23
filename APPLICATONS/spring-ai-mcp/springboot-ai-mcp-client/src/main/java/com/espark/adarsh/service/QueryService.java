79e67926032c04a0cdaf14b86049fe56ab4a7410
adarsh kumar <adarshkumarsingh83@gmail.com>
2025-08-23 17:15:35 -0500

    spring-ai-mcp

---
+++ APPLICATONS/spring-ai-mcp/springboot-ai-mcp-client/src/main/java/com/espark/adarsh/service/QueryService.java
@@ -0,0 +1,28 @@
+package com.espark.adarsh.service;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.stereotype.Service;
+
+@Service
+public class QueryService {
+
+    private final ChatClient chatClient;
+
+    public QueryService(ChatClient.Builder chatClientBuilder, ToolCallbackProvider tools) {
+
+        this.chatClient = chatClientBuilder
+                .defaultSystem("Please prioritise context information for answering questions")
+                .defaultToolCallbacks(tools)
+                .build();
+    }
+
+
+    public String chat(String query) {
+        PromptTemplate promptTemplate = new PromptTemplate(query);
+        Prompt prompt = promptTemplate.create();
+        ChatClient.CallResponseSpec res = chatClient.prompt(prompt).call();
+        return res.content();
+    }
+}
---
+++ APPLICATONS/spring-ai-mcp/springboot-ai-mcp-server/src/main/java/com/espark/adarsh/service/McpService.java
@@ -0,0 +1,31 @@
+package com.espark.adarsh.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
+
+@Slf4j
+@Service
+public class McpService {
+
+    @Tool(description = "Wish a user based on user name")
+    public String wishAction (String input) {
+        log.info("Input received in wishAction : {}",input);
+        return wishBasedOnTime.apply(input);
+    }
+
+    public Function<String,String> wishBasedOnTime = (String userName) -> {
+        int hour = java.time.LocalTime.now().getHour();
+        String greeting;
+        if (hour >= 5 && hour < 12) {
+            greeting = "Good Morning";
+        } else if (hour >= 12 && hour < 17) {
+            greeting = "Good Afternoon";
+        } else {
+            greeting = "Good Evening";
+        }
+        return greeting + " " + userName + ", How can I help you?";
+    };
+}
